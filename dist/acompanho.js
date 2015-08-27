104b0837c671f2516abbea1dd24546ba98b57a78
= <rafaelgil@outlook.com>
2015-08-27 09:42:44 -0300

    Fixed versions

--- dist/acompanho.js
+++ dist/acompanho.js
@@ -45,27 +45,6 @@ angular.module('Acompanho', [
   });
 });
 
-angular.module('Acompanho').filter('limitWordWise', function() {
-
-  'use strict';
-
-  return function(value, max) {
-    if (!value) return '';
-
-    max = parseInt(max, 10);
-    if (!max) return value;
-    if (value.length <= max) return value;
-
-    value = value.substr(0, max);
-    var lastspace = value.lastIndexOf(' ');
-    if (lastspace != -1) {
-      value = value.substr(0, lastspace);
-    }
-
-    return value + '…';
-  };
-});
-
 angular.module('Acompanho').controller('AcompanhoController', function($scope, $modal, $rootScope, $state, $q, $aside, FeedService, UserService, CategoryService) {
   'use strict';
 
@@ -334,4 +313,25 @@ angular.module('Acompanho').controller('NewFeedController', function($scope, $mo
     });
   };
   init();
+});
+
+angular.module('Acompanho').filter('limitWordWise', function() {
+
+  'use strict';
+
+  return function(value, max) {
+    if (!value) return '';
+
+    max = parseInt(max, 10);
+    if (!max) return value;
+    if (value.length <= max) return value;
+
+    value = value.substr(0, max);
+    var lastspace = value.lastIndexOf(' ');
+    if (lastspace != -1) {
+      value = value.substr(0, lastspace);
+    }
+
+    return value + '…';
+  };
 });
