6b0b272985c6ac7830a03d366aa61c7ac4105bc7
= <rafaelgil@outlook.com>
2015-09-08 18:58:09 -0300

    Gulp minify.

--- gulpfile.js
+++ gulpfile.js
@@ -8,12 +8,15 @@
   var nodemon = require('gulp-nodemon');
   var inject = require('gulp-inject');
   var mainBowerFiles = require('main-bower-files');
+  var filter = require('gulp-filter');
+
+  var scriptFilter = '**/*.js';
 
   var paths = {
     servicesModule: './public/js/services/services.js',
     serviceFiles: './public/js/services/!(services)*.js',
     mainModule: './public/js/main.js',
-    appJs: './public/js/!(services)**/*.js',
+    appJs: './public/js/!(services|min)**/*.js',
     vendor: mainBowerFiles()
   };
 
@@ -59,7 +62,33 @@
       .pipe(gulp.dest('public'));
   });
 
-  gulp.task('dist', ['js']);
+
+  gulp.task('inject-prod', function() {
+    var target = gulp.src('./public/index.html');
+    var vendor = gulp.src(paths.vendor);
+    var sources = gulp.src([paths.servicesModule, paths.serviceFiles, paths.mainModule, paths.appJs]);
+
+    return target.pipe(inject(
+        sources.pipe(concat('./app'))
+        .pipe(rename('app.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./public/js/min')), {
+          addRootSlash: false,
+          ignorePath: 'public'
+        }))
+      .pipe(inject(vendor.pipe(filter(scriptFilter))
+        .pipe(concat('./vendor'))
+        .pipe(rename('vendor.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./public/js/min')), {
+          name: 'vendor',
+          addRootSlash: false,
+          ignorePath: 'public'
+        }))
+      .pipe(gulp.dest('public'));
+  });
+
+  gulp.task('dist', ['js', 'inject-prod']);
 
   gulp.task('serve', ['inject-dev'], function() {
     nodemon({
