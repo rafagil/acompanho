e7f646419574fac2a7bb336ba950109c7ed02b69
Rafael Gil <rafaelgil@outlook.com>
2016-01-08 11:06:41 -0200

    Small bugfixes

--- public/js/controllers/UnreadEntriesController.js
+++ public/js/controllers/UnreadEntriesController.js
@@ -21,6 +21,16 @@ angular.module('Acompanho').controller('UnreadEntriesController', [
       });
     };
 
+    var updateUnreadCount = function(entryFeed) {
+      $scope.categories.forEach(function(cat) {
+        cat.feeds.forEach(function(feed) {
+          if (feed._id === entryFeed._id) {
+            feed.unreadCount--;
+          }
+        });
+      });
+    };
+
     $scope.openEntry = function(entry) {
       if (!entry.description) {
         entry.loading = true;
@@ -28,6 +38,7 @@ angular.module('Acompanho').controller('UnreadEntriesController', [
           entry.loading = false;
           entry.description = description;
           entry.unread = false;
+          updateUnreadCount(entry.feed);
           $scope.pageChanged(1);
         });
       }
@@ -35,9 +46,11 @@ angular.module('Acompanho').controller('UnreadEntriesController', [
     };
 
     $scope.pageChanged = function(pageNumber) {
+      $scope.loading = true;
       FeedService.allUnreadEntries(pageNumber, $scope.pageSize).then(function(entries) {
         $scope.entries = entries.entries;
         $scope.totalEntries = entries.total;
+        $scope.loading = false;
       });
     };
 
